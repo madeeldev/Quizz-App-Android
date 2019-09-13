496fbc50599956718bcf64ddf0924a7224916b65
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-14 02:30:14 +0500

    Casting Buttons and set Listening Events
    Casting Buttons and set Listening Events

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,6 +2,9 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,8 +12,8 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
-
-
+    Button mTrueButton;
+    Button mFalseButton;
     // TODO: Uncomment to create question bank
 //    private TrueFalse[] mQuestionBank = new TrueFalse[] {
 //            new TrueFalse(R.string.question_1, true),
@@ -33,7 +36,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
 
-
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Clicked: ", "True Button");
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Clicked: ", "False Button");
+            }
+        });
     }
 }
